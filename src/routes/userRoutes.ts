c66f5d3da91bc331a633504489b4ee8b1e95b7fa
felipe baloneker <felipebaloneker23@gmail.com>
2022-06-03 14:37:42 -0300

    feat

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { create, erase, index, update } from "../controller/UserController";
 
 const router = express.Router()
 
-
+router.post('/', create)
+router.get('/', index)
+router.put('/:email', update)
+router.delete('/:email', erase)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv'
 import express from 'express'
 
+import router from "./routes/userRoutes"
 import './database'
 
 const PORT = process.env.PORT || 3000
@@ -8,4 +9,6 @@ const PORT = process.env.PORT || 3000
 const server = express()
 
 server.use(express.json())
+server.use(router)
+
 server.listen(PORT, () => { console.log("server running") })
