02e66bad3b4fa6f7ab04e56d55507f4a32055655
felipe baloneker <felipebaloneker23@gmail.com>
2022-06-08 17:48:09 -0300

    updated

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 mongoose.connect(
     `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_MONGO_PORT}`,
-
-    (err) => { if (err) { throw err } }
+    {},
+    (res) => {
+        if (!res) {
+            console.log("Connected on MongoDb")
+        } else {
+            console.log("Error on Connect MongoDb: " + res)
+        }
+    }
 )
 mongoose.Promise = global.Promise
 export { mongoose }
