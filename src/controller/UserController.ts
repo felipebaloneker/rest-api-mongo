2410bb01723586fa1aacacf93b1e8b299806dd7c
felipe baloneker <felipebaloneker23@gmail.com>
2022-06-08 10:14:30 -0300

    updated

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,60 +1,66 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
-import { get, getOne, include, alter, exclude } from "../database/databaseController"
+import controllerDB from "../database/databaseController";
 
-export async function create(request: Request, response: Response) {
-    try {
-        const { name, email, password } = request.body
-        if (!name || !email || !password) {
-            return response.status(400).send({ error: "Registration Failed" })
-        }
-        const isUserExist = await getOne(email)
-        if (isUserExist) {
-            return response.status(400).send("User already exist")
-        }
-        const user = await include({ name, email, password })
-        return response.send({ user })
-    }
-    catch (err) {
-        return response.status(400).send({ err: "Registration Failed" })
-    }
-}
-
-export async function erase(request: Request, response: Response) {
-    try {
+const controllerCode = {
+    get: async (request: Request, response: Response) => {
         const email = request.params.email
-        const isUserExist = await getOne(email as string)
-        if (!isUserExist) {
-            return response.status(400).send({ error: "Error on Delete" });
+        try {
+            const user = await controllerDB.get()
+            return response.send({ user })
+
+        } catch (error) {
+            return response.status(400).send({ error: "Error on list" })
         }
-        const user = await exclude(email)
-        return response.send({ user })
+    },
+    create: async (request: Request, response: Response) => {
+        try {
+            const { name, email, password } = request.body
+            if (!name || !email || !password) {
+                return response.status(400).send({ error: "Registration Failed" })
+            }
+            const isUserExist = await controllerDB.getOne(email)
+            if (isUserExist) {
+                return response.status(400).send("User already exist")
+            }
+            const user = await controllerDB.create({ name, email, password })
+            return response.send({ user })
+        }
+        catch (err) {
+            return response.status(400).send({ err: "Registration Failed" })
+        }
+    },
+
+    update: async (request: Request, response: Response) => {
+        try {
+            const { email, name, password } = request.params
+            const isUserExist = await controllerDB.getOne(email)
+            if (isUserExist) {
+                await controllerDB.update(name, password)
+                const findNew = await controllerDB.getOne(email)
+                return response.send({ findNew })
+            }
+        } catch (error) {
+            return response.status(400).send({ error: "Error on update" })
+        }
+    },
+    delete: async (request: Request, response: Response) => {
+        try {
+            const email = request.params.email
+            const isUserExist = await controllerDB.getOne(email as string)
+            if (!isUserExist) {
+                return response.status(400).send({ error: "Error on Delete" });
+            }
+            const user = await controllerDB.delete(email)
+            return response.send({ user })
+        }
+        catch (error) {
+            return response.status(400).send({ error: "Delete Failed" })
+        }
     }
-    catch (error) {
-        return response.status(400).send({ error: "Delete Failed" })
-    }
+
+
+
 }
 
-export async function index(request: Request, response: Response) {
-    const email = request.params.email
-    try {
-        const user = await get()
-        return response.send({ user })
-
-    } catch (error) {
-        return response.status(400).send({ error: "Error on list" })
-    }
-}
-export async function update(request: Request, response: Response) {
-    try {
-        const { email, name, password } = request.params
-        const isUserExist = await getOne(email)
-        if (isUserExist) {
-            await alter(name, password)
-            const findNew = await getOne(email)
-            return response.send({ findNew })
-        }
-    } catch (error) {
-        return response.status(400).send({ error: "Error on update" })
-    }
-}
+export default controllerDB
--- src/database/databaseController.ts
+++ src/database/databaseController.ts
@@ -1,55 +1,58 @@
 import { IUser, User } from "../models/User";
 
-export async function get() {
-    try {
-        const user = await User.find({})
+const controllerDB = {
+    get: async () => {
+        try {
+            const user = await User.find({})
 
-        return user
-    }
-    catch (err) {
-        return err
+            return user
+        }
+        catch (err) {
+            return err
+        }
+    },
+    getOne: async (email: string) => {
+        try {
+            const user = await User.findOne({ email })
+
+            return user
+        }
+        catch (err) {
+            return err
+        }
+    },
+    create: async ({ name, email, password }: IUser) => {
+        try {
+            const user = await User.create({ name, email, password })
+
+            return user
+        }
+        catch (err) {
+            return err
+        }
+    },
+
+    delete: async (email: string) => {
+        try {
+            const user = await User.deleteOne({ email })
+
+            return user
+        }
+        catch (err) {
+            return err
+        }
+
+    },
+
+    update: async (name: string, password: string) => {
+        try {
+            const user = await User.updateOne({ name, password })
+
+            return user
+        } catch (error) {
+            return error
+        }
     }
 }
 
-export async function getOne(email: string) {
-    try {
-        const user = await User.findOne({ email })
-
-        return user
-    }
-    catch (err) {
-        return err
-    }
-}
-
-export async function include({ name, email, password }: IUser) {
-    try {
-        const user = await User.create({ name, email, password })
-
-        return user
-    }
-    catch (err) {
-        return err
-    }
-}
-
-export async function exclude(email: string) {
-    try {
-        const user = await User.deleteOne({ email })
-
-        return user
-    }
-    catch (err) {
-        return err
-    }
-
-}
-export async function alter(name: string, password: string) {
-    try {
-        const user = await User.updateOne({ name, password })
-
-        return user
-    } catch (error) {
-        return error
-    }
-}
+export default controllerDB
